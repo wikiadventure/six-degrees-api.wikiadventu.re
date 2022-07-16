e1ed10e7a2b99b6e78eba9f3a8043ed1d6850718
Sacramentix <benji.u1225u@gmail.com>
2022-07-16 14:23:00 +0200

    add no auth options

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,21 @@ import { Database } from 'arangojs';
 export const max_group_per_request = parseInt(process.env['MAX_GROUP_PER_REQUEST'] || "20000");
 const args = process.argv.slice(2);
 
-export const lang = args[0] || "en";
+export const lang = process.env['WIKI_LANG'];
 const DB_URL = process.env['DB_URL'];
 const DB_USERNAME = process.env['DB_USERNAME'];
 const DB_PASSWORD = process.env['DB_PASSWORD'];
 
-if (DB_URL == null || DB_USERNAME == null || DB_PASSWORD == null) throw "Fill env with database url, username and password!";
+if (lang == null || DB_URL == null) throw "Fill env with database url and lang";
 
-export const db = new Database({
+export const db = new Database(DB_USERNAME != null ? {
     url: DB_URL,
     auth: {
         username: DB_USERNAME,
         password: DB_PASSWORD
     }
+} : {
+    url: DB_URL
 });
 
 export const langDb = db.database(`${lang}wiki`);
