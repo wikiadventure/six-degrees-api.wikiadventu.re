5878f6c6524c0df961e1f43f23179cbe7b539011
Sacramentix <benji.u1225u@gmail.com>
2025-06-16 20:32:32 +0200

    take whole data folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ gsutil requesterpays set on gs://${WIKI_LANG}-wiki-graph-data &&
 gsutil iam ch allAuthenticatedUsers:objectViewer gs://${WIKI_LANG}-wiki-graph-data &&
 # # Clear the storage bucket before uploading new data
 gsutil -m rm -r gs://${WIKI_LANG}-wiki-graph-data/** || true &&
-gsutil -m cp -r "${NEO4J_HOME}/data/databases/*" gs://${WIKI_LANG}-wiki-graph-data &&
+gsutil -m cp -r "${NEO4J_HOME}/data/*" gs://${WIKI_LANG}-wiki-graph-data &&
 # Delete the Instance running this script
 export INSTANCE_NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google') &&\
 export INSTANCE_ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google') &&\
