6e2929535d5c68fe5429694165ba0ee95a2f53be
Sacramentix <benji.u1225u@gmail.com>
2025-06-17 18:23:06 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,1 @@
-cd /project/sql-dump-to-neo4j &&
-# Start Neo4j in background
-neo4j start  &&
-# Build and run the dump parser with Node js
-npm start &&
-# Push the content of $NEO4J_HOME/data to Google Cloud Storage
-# Create the Google Cloud Storage bucket if it doesn't exist
-gcloud config set project wikiadventure
-gsutil ls gs://${WIKI_LANG}-wiki-graph-data || gsutil mb -l eu gs://${WIKI_LANG}-wiki-graph-data &&
-gsutil requesterpays set on gs://${WIKI_LANG}-wiki-graph-data &&
-gsutil iam ch allAuthenticatedUsers:objectViewer gs://${WIKI_LANG}-wiki-graph-data &&
-# # Clear the storage bucket before uploading new data
-gsutil -m rm -r gs://${WIKI_LANG}-wiki-graph-data/* || true &&
-gsutil -m cp -r "${NEO4J_HOME}/data/*" gs://${WIKI_LANG}-wiki-graph-data &&
-# Delete the Instance running this script
-export INSTANCE_NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google') &&\
-export INSTANCE_ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google') &&\
-gcloud --quiet compute instances delete $INSTANCE_NAME --zone=$INSTANCE_ZONE
+cd /project/sql-dump-to-neo4j && npm start-GCP
